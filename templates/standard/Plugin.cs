752b34cb5820a573883845ec7b95f0b1afe4e23c
Daniel Solin <daniel@solin.org>
2025-08-10 21:35:50 +0200

    Moved templates to root. Removed plugin base class to simplify solution.

---
+++ templates/standard/Plugin.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.Xrm.Sdk;
+
+namespace PluginRight.Plugins
+{
+    public sealed class __CLASS_NAME__ : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+
+        }
+    }
+}
